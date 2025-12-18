cdf11f1aa57d247a7febc9eb36ad509f088d23c1
zihasyu <1045839067@qq.com>
2025-12-18 08:48:20 +0000

    init others' method out of line

---
+++ include/Offline/offline_treecache.h
@@ -0,0 +1,36 @@
+#ifndef OFFLINE_TREE_CACHE_H
+#define OFFLINE_TREE_CACHE_H
+
+#include "../absmethod.h"
+#include "../odess_similarity_detection.h"
+
+#include "../lruCache.h"
+
+#include <unordered_map>
+
+using namespace std;
+
+class OfflineTreeCache : public AbsMethod
+{
+private:
+    string myName_ = "OfflineTreeCache";
+    int PrevDedupChunkid = -1;
+    int Version = 0;
+    uint8_t *MinBaseBuffer = nullptr;
+    uint8_t *tmpDeltaBuffer = nullptr;
+
+    lru11::Cache<uint64_t, std::vector<uint8_t>, std::mutex> chunkCache;
+    size_t cacheHitCount = 0;
+    size_t cacheAccessCount = 0;
+    std::unordered_map<uint64_t, int> chunkHotMap;
+
+public:
+    OfflineTreeCache();
+    ~OfflineTreeCache();
+    void ProcessTrace();
+    Chunk_t CutGreedy(uint64_t BasechunkId, const Chunk_t Targetchunk, SuperFeatures sfs);
+    uint8_t *xd3_encode_buffer(const uint8_t *targetChunkbuffer, size_t targetChunkbuffer_size, const uint8_t *baseChunkBuffer, size_t baseChunkBuffer_size, size_t *deltaChunkBuffer_size, uint8_t *tmpbuffer);
+    void StatsHit(uint64_t FatherID, uint64_t HitID, SuperFeatures sfs);
+    Chunk_t xd3_recursive_restore_BL_time(uint64_t BasechunkId);
+};
+#endif
---
+++ include/Offline/offline_treecutlayer.h
@@ -0,0 +1,26 @@
+#ifndef OFFLINE_TREE_CUT_LAYER_H
+#define OFFLINE_TREE_CUT_LAYER_H
+
+#include "../absmethod.h"
+#include "../odess_similarity_detection.h"
+
+using namespace std;
+
+class OfflineTreeCutLayer : public AbsMethod
+{
+private:
+    string myName_ = "OfflineTreeCutLayer";
+    int PrevDedupChunkid = -1;
+    int Version = 0;
+    uint8_t *MinBaseBuffer = nullptr;
+    uint8_t *tmpDeltaBuffer = nullptr;
+
+public:
+    OfflineTreeCutLayer();
+    ~OfflineTreeCutLayer();
+    void ProcessTrace();
+    Chunk_t CutGreedy(uint64_t BasechunkId, const Chunk_t Targetchunk, SuperFeatures sfs);
+    uint8_t *xd3_encode_buffer(const uint8_t *targetChunkbuffer, size_t targetChunkbuffer_size, const uint8_t *baseChunkBuffer, size_t baseChunkBuffer_size, size_t *deltaChunkBuffer_size, uint8_t *tmpbuffer);
+    void StatsHit(uint64_t FatherID, uint64_t HitID, SuperFeatures sfs);
+};
+#endif
