a7d0910f9f41bf09d8bb8b472040c6615e368c4a
drink2017 <2321866490@qq.com>
2025-12-26 02:16:55 +0000

    clear bin/restoreFile between the experiment

--- Test1.sh
+++ Test1.sh
@@ -3,35 +3,42 @@ cd bin
 # 数据集路径和版本数
 declare -A datasets
 datasets=(
-  ["automake"]="/mnt/dataset2/automake_tarballs 100"
-  ["bash"]="/mnt/dataset2/bash_tarballs 44"
-  ["coreutils"]="/mnt/dataset2/coreutils_tarballs 28"
-  ["fdisk"]="/mnt/dataset2/fdisk_tarballs 22"
-  ["glibc"]="/mnt/dataset2/glibc_tarballs 100"
-  ["smalltalk"]="/mnt/dataset2/smalltalk_tarballs 40"
-  ["gcc"]="/mnt/dataset2/GNU_GCC/gcc-packed/tar 117"
-  ["chromium"]="/mnt/dataset2/chromium 107"
-  ["linux-100"]="/mnt/dataset2/linux 100"
-  ["linux"]="/mnt/dataset2/linux 270"
-  ["cassandra"]="/mnt/dataset2/cassandra 97"
-  ["vmdk"]="/mnt/dataset2/vmdk 8"
-  ["WEB"]="/mnt/dataset2/WEB 20"
-  ["WEB-3"]="/mnt/dataset2/WEB 3"
-  ["WindowsLog"]="/mnt/dataset2/WindowsLog 1"
-  ["ThunderbirdLog"]="/mnt/dataset2/ThunderbirdLog 1"
-  ["Wiki"]="/mnt/dataset2/wiki2025 7"
+  # ["automake"]="/mnt/dataset2/automake_tarballs 100"
+  # ["bash"]="/mnt/dataset2/bash_tarballs 44"
+  # ["coreutils"]="/mnt/dataset2/coreutils_tarballs 28"
+  # ["fdisk"]="/mnt/dataset2/fdisk_tarballs 22"
+  ["glibc"]="/home/public/Dataset/glibc_tarballs/glibc_tarballs 100"
+  # ["smalltalk"]="/mnt/dataset2/smalltalk_tarballs 40"
+  # ["gcc"]="/mnt/dataset2/GNU_GCC/gcc-packed/tar 117"
+  # ["chromium"]="/mnt/dataset2/chromium 107"
+  # ["linux-100"]="/mnt/dataset2/linux 100"
+  ["linux"]="/home/public/Dataset/linux 270"
+  # ["cassandra"]="/mnt/dataset2/cassandra 97"
+  # ["vmdk"]="/mnt/dataset2/vmdk 8"
+  # ["WEB"]="/mnt/dataset2/WEB 20"
+  ["WEB-3"]="/home/public/Dataset/WEB 3"
+  ["WindowsLog"]="/home/public/Dataset/WindowsLog 1"
+  # ["ThunderbirdLog"]="/mnt/dataset2/ThunderbirdLog 1"
+  # ["Wiki"]="/mnt/dataset2/wiki2025 7"
 )
 
 # 实验组合列表：数据集 分块方法 在线方法 离线方法 是否恢复
 experiments=(
-  "glibc 1 3 -1 0"
+  "glibc 1 3 -1 1"
   "glibc 1 3 0 1"
-  "automake 1 12 -1 0"
-  "automake 1 12 1 1"
+  "linux 1 3 -1 1"
+  "linux 1 3 0 1"
+  "WEB-3 1 3 -1 1"
+  "WEB-3 1 3 0 1"
+  "WindowsLog 1 3 -1 1"
+  "WindowsLog 1 3 0 1"
   # ...
 )
 
 for exp in "${experiments[@]}"; do
+  # 清空 restoreFile 文件夹内容
+  rm -rf restoreFile/*
+
   read -r dataset chunking online offline restore <<< "$exp"
   read -r path num <<< "${datasets[$dataset]}"
   offline_arg=""
--- Test2.sh
+++ Test2.sh
@@ -2,23 +2,23 @@ cd bin
 
 declare -A datasets
 datasets=(
-#   ["automake"]="/mnt/dataset2/automake_tarballs 100"
-#   ["bash"]="/mnt/dataset2/bash_tarballs 44"
-#   ["coreutils"]="/mnt/dataset2/coreutils_tarballs 28"
-#   ["fdisk"]="/mnt/dataset2/fdisk_tarballs 22"
-  ["glibc"]="/mnt/dataset2/glibc_tarballs 100"
-#   ["smalltalk"]="/mnt/dataset2/smalltalk_tarballs 40"
-#   ["gcc"]="/mnt/dataset2/GNU_GCC/gcc-packed/tar 117"
-#   ["chromium"]="/mnt/dataset2/chromium 107"
-#   ["linux-100"]="/mnt/dataset2/linux 100"
-  ["linux"]="/mnt/dataset2/linux 270"
-#   ["cassandra"]="/mnt/dataset2/cassandra 97"
-#   ["vmdk"]="/mnt/dataset2/vmdk 8"
-  ["WEB"]="/mnt/dataset2/WEB 20"
-  ["WEB-3"]="/mnt/dataset2/WEB 3"
-  ["WindowsLog"]="/mnt/dataset2/WindowsLog 1"
-#   ["ThunderbirdLog"]="/mnt/dataset2/ThunderbirdLog 1"
-#   ["Wiki"]="/mnt/dataset2/wiki2025 7"
+  # ["automake"]="/mnt/dataset2/automake_tarballs 100"
+  # ["bash"]="/mnt/dataset2/bash_tarballs 44"
+  # ["coreutils"]="/mnt/dataset2/coreutils_tarballs 28"
+  # ["fdisk"]="/mnt/dataset2/fdisk_tarballs 22"
+  ["glibc"]="/home/public/Dataset/glibc_tarballs/glibc_tarballs 100"
+  # ["smalltalk"]="/mnt/dataset2/smalltalk_tarballs 40"
+  # ["gcc"]="/mnt/dataset2/GNU_GCC/gcc-packed/tar 117"
+  # ["chromium"]="/mnt/dataset2/chromium 107"
+  # ["linux-100"]="/mnt/dataset2/linux 100"
+  ["linux"]="/home/public/Dataset/linux 270"
+  # ["cassandra"]="/mnt/dataset2/cassandra 97"
+  # ["vmdk"]="/mnt/dataset2/vmdk 8"
+  # ["WEB"]="/mnt/dataset2/WEB 20"
+  ["WEB-3"]="/home/public/Dataset/WEB 3"
+  ["WindowsLog"]="/home/public/Dataset/WindowsLog 1"
+  # ["ThunderbirdLog"]="/mnt/dataset2/ThunderbirdLog 1"
+  # ["Wiki"]="/mnt/dataset2/wiki2025 7"
 )
 
 chunking_methods=(1)           # 分块方法列表
@@ -32,6 +32,9 @@ for dataset in "${!datasets[@]}"; do
     for online in "${online_methods[@]}"; do
       for offline in "${offline_methods[@]}"; do
         for restore in "${restore_options[@]}"; do
+          # 清空 restoreFile 文件夹内容
+          rm -rf restoreFile/*
+
           offline_arg=""
           outname=""
           if [[ $offline -ge 0 ]]; then
