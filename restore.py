dfc5d013db063ffeb14abedf9f32c2b9f622a010
drink2017 <2321866490@qq.com>
2026-01-30 20:56:45 +0800

    generate data with python script

---
+++ restore.py
@@ -0,0 +1,18 @@
+import re
+
+input_file = 'bin/C1_M3_linux_R1_T64.txt'
+output_file = 'restore/linux_odess.txt'
+
+restore_times = []
+
+with open(input_file, 'r', encoding='utf-8') as f:
+    for line in f:
+        match = re.search(r'Restore time:\s*([\d\.]+)', line)
+        if match:
+            restore_times.append(match.group(1))
+
+with open(output_file, 'w', encoding='utf-8') as f:
+    for t in restore_times:
+        f.write(f"{t}\n")
+
+print(f"已提取{len(restore_times)}个恢复时间到 {output_file}")
